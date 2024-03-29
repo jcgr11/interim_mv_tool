2a950115738247d5fc0fde9e454ea7d975778032
Julio Rengifo <126374722+jcgr11@users.noreply.github.com>
2024-03-29 14:30:09 -0400

    updated main function to correct the streamlitapp method call. updated instruction to run locally

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from streamlit_app import StreamlitApp
 
 def main():
     app = StreamlitApp()
-    app.run()
+    app.display_results()
 
 if __name__ == "__main__":
     main()
 
-# To run locally enter "streamlit run interim_mv_tool.py" in terminal and press enter.
+# To run locally enter "streamlit run C:/your_path/main.py" in terminal and press enter.
